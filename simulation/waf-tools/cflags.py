e11ebe6a07da486975e812370e50f3ee14943800
rfrantz <rfrantz@nvidia.com>
2025-01-17 12:05:45 +0100

    [build] Move to C++17
    C++17 is necessary for the toml library.

--- simulation/waf-tools/cflags.py
+++ simulation/waf-tools/cflags.py
@@ -23,7 +23,7 @@ class GccTraits(CompilerTraits):
 		self.warnings_flags = []
 
 	def get_cxx_version_flags(self):
-		return ['-std=gnu++11']
+		return ['-std=c++17']
 
 	def get_warnings_flags(self, level):
 		warnings = []
