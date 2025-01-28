d1cd9ea8a156d3940e28ff788b26cce0f15d2033
rfrantz <rfrantz@nvidia.com>
2025-01-28 15:54:44 +0100

    refactor: Remove dead code in `RdmaDriver::Init()`

--- simulation/src/rdma/model/rdma-driver.cc
+++ simulation/src/rdma/model/rdma-driver.cc
@@ -18,18 +18,8 @@ TypeId RdmaDriver::GetTypeId (void)
 RdmaDriver::RdmaDriver(){
 }
 
-void RdmaDriver::Init(void){
-	Ptr<Ipv4> ipv4 = m_node->GetObject<Ipv4> ();
-	#if 0
-	m_rdma->m_nic.resize(ipv4->GetNInterfaces());
-	for (uint32_t i = 0; i < m_rdma->m_nic.size(); i++){
-		m_rdma->m_nic[i] = CreateObject<RdmaQueuePairGroup>();
-		// share the queue pair group with NIC
-		if (IsQbb(ipv4->GetNetDevice(i))){
-			DynamicCast<QbbNetDevice>(ipv4->GetNetDevice(i))->m_rdmaEQ->m_qpGrp = m_rdma->m_nic[i];
-		}
-	}
-	#endif
+void RdmaDriver::Init() {
+
 	for (uint32_t i = 0; i < m_node->GetNDevices(); i++){
 		Ptr<QbbNetDevice> dev = NULL;
 		if (IsQbb(m_node->GetDevice(i)))
@@ -37,16 +27,7 @@ void RdmaDriver::Init(void){
 		m_rdma->m_nic.push_back(RdmaInterfaceMgr(dev));
 		m_rdma->m_nic.back().qpGrp = CreateObject<RdmaQueuePairGroup>();
 	}	
-	#if 0
-	for (uint32_t i = 0; i < ipv4->GetNInterfaces (); i++){
-		if (IsQbb(ipv4->GetNetDevice(i)) && ipv4->IsUp(i)){
-			Ptr<QbbNetDevice> dev = DynamicCast<QbbNetDevice>(ipv4->GetNetDevice(i));
-			// add a new RdmaInterfaceMgr for this device
-			m_rdma->m_nic.push_back(RdmaInterfaceMgr(dev));
-			m_rdma->m_nic.back().qpGrp = CreateObject<RdmaQueuePairGroup>();
-		}
-	}
-	#endif
+
 	// RdmaHw do setup
 	m_rdma->SetNode(m_node);
 	m_rdma->Setup(MakeCallback(&RdmaDriver::QpComplete, this));
