4380dca78840b0865fd13597db4225cf420adf9e
Raphaël Frantz <frantze.raphael@gmail.com>
2025-05-18 17:03:48 +0000

    docs:

--- analysis/src/project.py
+++ analysis/src/project.py
@@ -3,17 +3,29 @@ import os
 import git
 
 """
-Get important directories of the git project
-And convert to container paths
+Get important directories of the git project ns3-roce.
+Permits to convert between host paths and container paths.
 """
+
+
+def root_path() -> pathlib.Path:
+    """
+    Gets the git root directory absolute path.
+    """
+    repo = git.Repo('.', search_parent_directories=True)
+    return pathlib.Path(repo.working_tree_dir)
 
 
 def dir_from_root(path: pathlib.Path, create: bool = True):
     """
-    Get a directory from the git root.
-    The directory should exist.
+    Converts the path of a directory from relative to the git root to an absolute path.
+
     Params:
-        path: Path relative to the git root.
+        path:
+            Relative path to get from the git root.
+        create:
+            If false, the directory should exist.
+            If true, create the directory if it doesn't exist.
     """
     path = root_path() / path
     if not path.is_dir():
@@ -25,20 +37,23 @@ def dir_from_root(path: pathlib.Path, create: bool = True):
 
 
 def analysis_path():
+    """
+    Gets the analysis directory.
+    """
     return dir_from_root("analysis")
 
 
 def out_path():
+    """
+    Gets the directory where to store simulation outputs.
+    """
     return dir_from_root("analysis/out", create=True)
 
 
-def root_path() -> pathlib.Path:
-    """Get the git root directory."""
-    repo = git.Repo('.', search_parent_directories=True)
-    return pathlib.Path(repo.working_tree_dir)
-
-
 def cd_to_script_dir() -> None:
+    """
+    Changes the working directory to the analysis' directory containing Python top-folder module. 
+    """
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
@@ -46,9 +61,8 @@ def cd_to_script_dir() -> None:
 
 def get_path_rel_to_container(path: pathlib.Path) -> str:
     """
-    Convert a host path to a container path.
-    The path should be mounted in the container.
-    Root of the git project is mounted in container.
+    Converts from a host path to a container path.
+    Throws an error if the given `path` does not point into the git project.
     """
     if not path.is_relative_to(root_path()):
         raise Exception("Path should be inside git directory.")
--- analysis/src/pyutils.py
+++ analysis/src/pyutils.py
@@ -14,7 +14,11 @@ import seaborn as sns
 import pandas as pd
 
 class NpEncoder(json.JSONEncoder):
-    """https://stackoverflow.com/questions/50916422/python-typeerror-object-of-type-int64-is-not-json-serializable"""
+    """
+    Used internally by this module to (de)serialize Python objects containing numpy objects transparently.
+    By default, some types are not supported.
+    See https://stackoverflow.com/questions/50916422/python-typeerror-object-of-type-int64-is-not-json-serializable.
+    """
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
@@ -27,45 +31,73 @@ class NpEncoder(json.JSONEncoder):
         return super().default(obj)
     
 
-def dump_json(data, indent=4):
+def dump_json(data, indent=4) -> str:
+    """
+    Serializes a Python object into a JSON string.
+    """
     return json.dumps(data, cls=NpEncoder, indent=indent)
 
 def write_to_file(file: pathlib.Path, content: str) -> None:
+    """
+    Writes a string to a file.
+    Erases the file if the file already exists.
+    """
     with open(file, "w") as text_file:
         text_file.write(content)
 
 
 def read_all_file(file: pathlib.Path) -> str:
+    """
+    Reads all the content of a file into a string.
+    """
     with open(file, "r") as text_file:
         return text_file.read()
     raise Exception(f"Cannot read file {file}")
 
 
-def load_json(file: pathlib.Path) -> str:
+def load_json(file: pathlib.Path) -> dict:
+    """
+    Reads all the content of a JSON file `.json` into a dictionary.
+    """
     with open(file, "r") as text_file:
         return json.load(text_file)
     raise Exception(f"Cannot read file {file}")
 
 
-def build_template_file(in_path: str, vars: dict, out_dir: str = None):
+def build_template_file(in_path: str, vars: dict, out_dir: str = None) -> tempfile.NamedTemporaryFile:
     """
-    Read a jinja2 template from `in_path` and build it into a NamedTemporaryFile which is returned.
-    The temporary file parent directory is this python file directory.
-    Params:
-        vars: List of key-value variables for the jinja template. Value is stringified automatically.
+    Builds a jinja2 template and stores the output into a temporary file.
+    The steps are:
+    - Read a jinja2 template from `in_path`
+    - Builds the template
+    - Stores it into a NamedTemporaryFile which is returned.
+
+    Parameters:
+        in_path:
+            Path to the input `.jinja2` template.
+        vars:
+            List of key-value variables for the jinja template.
+        out_dir:
+            Where to generate the built template.
+            If `None`, the directory of the generated file is the containing directory of this module.
     """
 
+    # Default of `out_dir` if not specified.
     if out_dir is None:
         out_dir = pathlib.Path(__file__).parent.as_posix()
 
+    # Ensures `out_dir` exists.
     out_dir.mkdir(parents=True, exist_ok=True)
 
     # Make output temporary file in current directory
     # Needed because Docker cannot acces host"s `/tmp` directory.
     out_file = tempfile.NamedTemporaryFile(dir=out_dir)
     print(f"Build template in_file='{in_path}' out_file='{out_file.name}'")
-    # Jinja do not take absolute path, so hack around.
+
+    # Jinja do not take absolute paths, so hack around.
     jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=in_path.parent.as_posix()))
+
+    # Build the template
     jinja_template= jinja_env.get_template(pathlib.Path(in_path).name)
     out_content = jinja_template.render(vars)
     out_file.write(out_content.encode())
@@ -74,19 +106,28 @@ def build_template_file(in_path: str, vars: dict, out_dir: str = None):
     return out_file
 
 
-def print_each_line(line: str) -> None:
+def print(line: str) -> None:
+    """
+    Just prints `line` to the standard output.
+
+    Can be used as a callback for `run_process`.
+    """
     print(line, end="")
 
 
 def run_process(argv: list, each_line=None) -> None:
     """
-    Run a process.
+    Runs a process.
+    The command is stored in `argv[0]` and each argument in the rest of the elements.
     Raise an exception if the process exists with failure.
-    Params:
-        argv: List of arguments. First element is the command itself.
-        each_line: If not None, callback to be called for each line printed by the process (includes the newline character).
-    Returns:
-        None.
+
+    Parameters:
+        argv:
+            List of arguments.
+            First element is the command itself.
+        each_line:
+            If not None, callback to be called for each line printed by the process (includes the newline character).
+            With as single argument the current line as a string.
     """
     print(f"Running {argv}")
     proc = sp.Popen(argv, stdout=sp.PIPE, stderr=sp.STDOUT)
@@ -104,6 +145,15 @@ def run_process(argv: list, each_line=None) -> None:
 
 
 def parallel_for(data: list, func, jobs: int = 0):
+    """
+    Runs a function in parallel.
+    `func(data[0])`, `func(data[1])`, etc... are run in parallel.
+
+    Parameters:
+        data: Each element is fed as the argument of `func`, for each execution.
+        func: The function to execute.
+        jobs: Maximum count of parallel jobs. If zero or negative, then choose automatically.
+    """
     # Some bugs with loky backend and rich.progress.
     # We actually don't need parallel python code,
     # because we spawn a new Process for each task.
@@ -112,13 +162,17 @@ def parallel_for(data: list, func, jobs: int = 0):
 
 
 def read_avro(path: pathlib.Path) -> pd.DataFrame:
-  """Read an avro dataset into a pandas DataFrame"""
+  """
+  Reads an avro dataset into a pandas DataFrame.
+  """
   with open(path, "rb") as file:
     reader = DataFileReader(file, DatumReader())
     return pd.DataFrame.from_records([r for r in reader])
   
 
 def scientific_axis(g: sns.FacetGrid) -> None:
-   """Display a matplotlib axis as scientific notation"""
+   """
+   Displays the axis unit of a seaborn `FacetGrid` in scientific notation.
+   """
    for axes in g.axes.flat:
       axes.ticklabel_format(axis='both', style='scientific', scilimits=(0, 0))
